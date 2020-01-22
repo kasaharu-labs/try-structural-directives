2e5d07b8ef54b4c1fc3269d56b5f580cf010534d
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-22 09:53:22 +0900

    yarn ng g directive unless

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UnlessDirective } from './unless.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UnlessDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
