9e84a352baec7e56d2d6db19aa74588d80c42c75
Wataru.Kasahara <kasahara1019@gmail.com>
2020-01-22 10:23:32 +0900

    appUnless ディレクティブの実装

--- src/app/unless.directive.ts
+++ src/app/unless.directive.ts
@@ -1,8 +1,22 @@
-import { Directive, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appUnless]',
 })
 export class UnlessDirective {
   constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {}
+
+  // tslint:disable-next-line: cyclomatic-complexity
+  @Input()
+  set appUnless(condition: boolean) {
+    if (!condition && !this.hasView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else if (condition && this.hasView) {
+      this.viewContainer.clear();
+      this.hasView = false;
+    }
+  }
+
+  private hasView = false;
 }
